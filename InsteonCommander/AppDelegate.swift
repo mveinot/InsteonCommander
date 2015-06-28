54f73c889409959b773f414d9350386e45da88aa
Mark Veinot <mveinot@gmail.com>
2015-06-28 07:45:10 -0300

    Initial Commit

---
+++ InsteonCommander/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  InsteonCommander
+//
+//  Created by Mark Veinot on 2015-06-28.
+//  Copyright (c) 2015 Mark Veinot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
